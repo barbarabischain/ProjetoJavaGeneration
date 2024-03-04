246c3d2ccb419e324812cb363aa6d1a7f9991ca0
barbarabischain <bbischain@gmail.com>
2024-03-04 16:21:53 -0300

    exemplo

--- src/metodos/ExemploMetodo.java
+++ src/metodos/ExemploMetodo.java
@@ -1,10 +1,16 @@
 package metodos;
 
-public class ExemploMetodo {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class ExemploMetodo
+{
+	public static void main(String[] args)
+	{
+		System.out.println(somar(3,2));
 	}
-
+	//valor1 + valor2 = resultado
+	public static int somar(int	num1, int num2)
+	{
+		return (num1 + num2);
+	}
 }
